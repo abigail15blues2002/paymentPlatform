fe749e69b7037ebb1cafadeefe5c8a40d619eb55
abigail15blues2002 <abigail.anguluan@gmail.com>
2025-08-02 12:41:26 +1000

    implement getPayment (#1) with checking if no matching payment found in database (#3)

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,5 +1,34 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getPayment } from './lib/payments';
+
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    let paymentId = undefined;
+    try {
+        paymentId = event.pathParameters?.id;
+        if (!paymentId) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ message: 'Missing payment id' }),
+            };
+        }
+        const payment = await getPayment(paymentId);
+        if (!payment) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Payment not found' }),
+            };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(payment),
+        };
+    } catch (error) {
+        // Log paymentId for debugging
+        console.error('Error in getPayment handler:', { error, paymentId });
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal server error' }),
+        };
+    }
 };
